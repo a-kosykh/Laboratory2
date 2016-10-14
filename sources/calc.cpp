99ef2ee2e8e551b3d672ff1384db995cc780db25
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-14 14:05:03 +0400

    Create calc.cpp

---
+++ sources/calc.cpp
@@ -0,0 +1,39 @@
+long double sum(float x, float y)
+{
+    return (x+y);
+}
+long double div(float x, float y)
+{
+    if (y != 0)
+        return (x/y);
+    else {
+        return (x/y);
+    }
+}
+long double multi(float x, float y)
+{
+    return (x*y);
+}
+long double min(float x, float y)
+{
+    return (x-y);
+}
+long double pow(float x, float y) {
+    float power;
+    int i;
+    power = 1;
+    for (i = 1; i <= y; i++) {
+        power = power * x;
+    }
+    return power;
+}
+long double sqrt(float x) {
+    double powi = 1;
+    int i = 1;
+    for(i;i<x;i++)
+    {
+        if (pow(powi,2) == x) break;
+        else (powi = 0.5*(powi + x/powi));
+    }
+    return powi;
+}

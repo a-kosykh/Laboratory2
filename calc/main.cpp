a6e94324b9a1535f2be60daeffb3c3402fe89a29
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-14 17:42:26 +0300

    Update main.cpp

--- calc/main.cpp
+++ calc/main.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
 #include <"calc.hpp">
 using namespace std;
-long double sum(float x, float y) {
-    return (x+y);
-}
-long double div(float x, float y) {
-    if (y != 0)
-        return (x/y);
-    else {
-        return (x/y);
-    }
-}
-long double multi(float x, float y) {
-    return (x*y);
-}
-long double min(float x, float y) {
-    return (x-y);
-}
-long double pow(float x, float y) {
-    float power;
-    int i;
-    power = 1;
-    for (i = 1; i <= y; i++) {
-        power = power * x;
-    }
-    return power;
-}
-long double sqrt(float x) {
-    double powi=1; double y;
-    int i=0;
-    while(1) {
-        y=powi;
-        powi=1./2*(powi+x/powi);
-        if (powi>=y) { i++; if (i>1) {break;};};
-    }
-    return(powi);
-}
+
 int main() {
     float a,b;
     cout << "a = ";

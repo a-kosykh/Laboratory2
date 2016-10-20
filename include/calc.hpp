4990a13af18dfd5fc03f359fdbaeb3985c7a3670
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-21 01:48:51 +0300

    Update calc.hpp

--- include/calc.hpp
+++ include/calc.hpp
@@ -1,6 +1,6 @@
-long double sum(float x, float y)
-long double div(float x, float y)
-long double multi(float x, float y)
-long double min(float x, float y)
-long double pow(float x, float y)
-long double sqrt(float x)
+long double sum(float x, float y);
+long double div(float x, float y);
+long double multi(float x, float y);
+long double min(float x, float y);
+long double pow(float x, float y);
+long double sqrt(float x);

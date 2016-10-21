a7504b3fe73bda53220211048873625bbb1b87d4
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-21 14:19:17 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,42 +2,42 @@
 #include <catch.hpp>
 
 SCENARIO ("calc sum", "[sum]"){
-    int x = 3;
-    int y = 4;
+    float x = 3;
+    float y = 4;
 
-    int rv = sum ( x,y );
+    double long rv = sum ( x,y );
         REQUIRE ( rv == 7 );
 }
 SCENARIO ("calc div", "[div]"){
-    int x = 16;
-    int y = 4;
+    float x = 16;
+    float y = 4;
 
-    int rv = div ( x,y );
+    double long rv = div ( x,y );
         REQUIRE ( rv == 4 );
 }
 SCENARIO ("calc multi", "[multi]"){
-    int x = 4;
-    int y = 6;
+    float x = 4;
+    float y = 6;
 
-    int rv = multi ( x,y );
+    double long rv = multi ( x,y );
         REQUIRE ( rv == 24 );
 }
 SCENARIO ("calc min", "[min]"){
-    int x = 12;
-    int y = 4;
+    float x = 12;
+    float y = 4;
 
-    int rv = min ( x,y );
+    double long rv = min ( x,y );
         REQUIRE ( rv == 8 );
 }
 SCENARIO ("calc pow", "[pow]"){
-    int x = 2;
-    int y = 3;
+    float x = 2;
+    float y = 3;
 
-    int rv = pow ( x,y );
+    double long rv = pow ( x,y );
         REQUIRE ( rv == 8 );
 }
 SCENARIO ("calc sqrt", "[sqrt]"){
-    int x = 16;
-    int rv = sqrt (x);
+    float x = 16;
+    double rv = sqrt (x);
         REQUIRE ( rv == 4 );
 }

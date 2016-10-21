7c62caa2bf141de70c8417cc71b04cebf1e2d222
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-21 14:08:35 +0400

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-    float a,b;
-    cout << "a = ";
-    cin >> a;
-    cout << "b = ";
-    cin >> b;
-    cout << "Сумма: " << sum(a,b) << endl;
-    cout << "Деление: " << div(a,b) << endl;
-    cout << "Умножение: " << multi(a,b) << endl;
-    cout << "Вычитание: " << min(a,b) << endl;
-    cout << "Степень: " << pow(a,b) << endl;
-    cout << "Квадратный корень первого числа: " << sqrt(a) << endl;
+    float x,y;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    cout << "Сумма: " << sum(x,y) << endl;
+    cout << "Деление: " << div(x,y) << endl;
+    cout << "Умножение: " << multi(x,y) << endl;
+    cout << "Вычитание: " << min(x,y) << endl;
+    cout << "Степень: " << pow(x,y) << endl;
+    cout << "Квадратный корень первого числа: " << sqrt(x) << endl;
 
     return 0;
 }

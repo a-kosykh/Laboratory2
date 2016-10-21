5c49d84582519379f5aa29dba68d3fbbd1e30938
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-21 14:33:32 +0400

    Update calc.hpp

--- include/calc.hpp
+++ include/calc.hpp
@@ -2,5 +2,5 @@ long double sum(float x, float y);
 long double div(float x, float y);
 long double multi(float x, float y);
 long double min(float x, float y);
-long double pow(float x, float y);
-long double sqrt(float x);
+long double power(float x, float y);
+long double sqrt1(float x);

f6d21827a7946479fad9e5789e452b7f63db0b36
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-21 14:07:09 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,42 +2,42 @@
 #include <catch.hpp>
 
 SCENARIO ("calc sum", "[sum]"){
-    int a = 3;
-    int b = 4;
+    int x = 3;
+    int y = 4;
 
     int rv = sum ( x,y );
         REQUIRE ( rv == 7 );
 }
 SCENARIO ("calc div", "[div]"){
-    int a = 16;
-    int b = 4;
+    int x = 16;
+    int y = 4;
 
     int rv = div ( x,y );
         REQUIRE ( rv == 4 );
 }
 SCENARIO ("calc multi", "[multi]"){
-    int a = 4;
-    int b = 6;
+    int x = 4;
+    int y = 6;
 
     int rv = multi ( x,y );
         REQUIRE ( rv == 24 );
 }
 SCENARIO ("calc min", "[min]"){
-    int a = 12;
-    int b = 4;
+    int x = 12;
+    int y = 4;
 
     int rv = min ( x,y );
         REQUIRE ( rv == 8 );
 }
 SCENARIO ("calc pow", "[pow]"){
-    int a = 2;
-    int b = 3;
+    int x = 2;
+    int y = 3;
 
     int rv = pow ( x,y );
         REQUIRE ( rv == 8 );
 }
 SCENARIO ("calc sqrt", "[sqrt]"){
-    int a = 16;
+    int x = 16;
     int rv = sqrt (x);
         REQUIRE ( rv == 4 );
 }

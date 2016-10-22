10f2e8a1c0500c0d4857a6eb85e9258dc6dce878
Aleksandr Kosykh <kik9399@mail.ru>
2016-10-22 17:36:27 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -9,12 +9,12 @@ int main() {
     cin >> x;
     cout << "y = ";
     cin >> y;
-    cout << "Сумма: " << sum(float x, float y) << endl;
-    cout << "Деление: " << div(float x, float y) << endl;
-    cout << "Умножение: " << multi(float x, float y) << endl;
-    cout << "Вычитание: " << min(float x, float y) << endl;
-    cout << "Степень: " << pow1(float x, float y) << endl;
-    cout << "Квадратный корень первого числа: " << sqrt1(float x) << endl;
+    cout << "Сумма: " << sum((float)x,(float)y) << endl;
+    cout << "Деление: " << div(float)x,(float)y) << endl;
+    cout << "Умножение: " << multi(float)x,(float)y) << endl;
+    cout << "Вычитание: " << min(float)x,(float)y) << endl;
+    cout << "Степень: " << pow1(float)x,(float)y) << endl;
+    cout << "Квадратный корень первого числа: " << sqrt1((float)x) << endl;
 
     return 0;
 }

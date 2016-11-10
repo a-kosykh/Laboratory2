c2855180954611d98e72fdeed7c335016f97fb6d
Aleksandr Kosykh <kik9399@mail.ru>
2016-11-10 19:29:57 +0300

    Update calc.cpp

--- sources/calc.cpp
+++ sources/calc.cpp
@@ -1,18 +1,18 @@
 long double sum(float x, float y)
 {
-    return (x+y);
+    return x+y;
 }
 long double div(float x, float y)
 {
-    return (x/y);
+    return x/y;
 }
 long double multi(float x, float y)
 {
-    return (x*y);
+    return x*y;
 }
 long double min(float x, float y)
 {
-    return (x-y);
+    return x-y;
 }
 long double pow1(float x, float y) {
     float power;
@@ -24,15 +24,15 @@ long double pow1(float x, float y) {
     return power;
 }
 long double sqrt1(float x) {
-    double powi=1; double y;
+    double temp=1; double y;
     int i=0;
     while(1) {
-        y=powi;
-        powi=0.5*(powi+x/powi);
-        if (powi>=y) {
+        y=temp;
+        temp=0.5*(temp+x/temp);
+        if (temp>=y) {
             i++; if (i>1);
             break;
         }
     }
-    return(powi);
+    return(temp);
 }

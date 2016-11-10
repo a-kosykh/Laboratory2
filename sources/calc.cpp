ef58f0dbec9acb576a8b42c2d37d43da84bc1641
Aleksandr Kosykh <kik9399@mail.ru>
2016-11-11 00:25:09 +0300

    Update calc.cpp

--- sources/calc.cpp
+++ sources/calc.cpp
@@ -29,19 +29,19 @@ long double pow1(float x, float y) {
     return power;
 }
 long double sqrt1(float x) {
-    float xn = 1.0;
-    float xn1 = 2.0;
-    if ( a != 1 && a != 0 )
+    float an = 1.0;
+    float an1 = 2.0;
+    if ( x != 1 && x != 0 )
     {
-        while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
+        while ( (an1 - an) > 0.000001 || (an1 - an) < -0.000001  )
         {
-            xn = xn1;
-            xn1 = (float)(xn + (float)a/xn)/2;
+            an = an1;
+            an1 = (float)(an + (float)x/an)/2;
         }
     }
-    else if ( a == 1 ) xn1 = 1;
-    else if ( a == 0 ) xn1 = 0;
+    else if ( x == 1 ) an1 = 1;
+    else if ( x == 0 ) an1 = 0;
 
 
-    return xn1;
+    return an1;
 }
